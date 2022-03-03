cf8cede99792bcf76148dcc9a39b283f56f0804d
Guilherme Batista <guilhermemalka6@gmail.com>
2022-03-03 16:32:51 -0300

    add label for student personal infomration and add js script for change picture

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ var i;
 // List View
 function listView() {
   for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "100%";
+    elements[i].style.width = "65%";
   }
 }
 
 // Grid View
 function gridView() {
   for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "25%";
+    elements[i].style.width = "23%";
   }
 }
 
+//change image on click
+
+var loadFile = function (event) {
+  var image = document.getElementById("output");
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
